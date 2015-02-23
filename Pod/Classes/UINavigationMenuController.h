062545e769c1aa7ca8ff34469aa17e592fb1bf3d
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-23 15:13:34 +0200

    Added class 'UINavigationMenuController'

---
+++ Pod/Classes/UINavigationMenuController.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationMenuController.h
+//  Pods
+//
+//  Created by Ilter Cengiz on 23/02/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationMenuController : UINavigationController
+
+@end
