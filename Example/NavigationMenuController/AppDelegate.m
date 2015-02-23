2055c3bffbea1c7a2c0fcba14fbb010fd718a479
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-23 15:12:40 +0200

    Initial project setup

--- Example/NavigationMenuController/AppDelegate.m
+++ Example/NavigationMenuController/AppDelegate.m
@@ -6,16 +6,21 @@
 //  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
 //
 
-#import "UIAppDelegate.h"
+#import "AppDelegate.h"
 
-@implementation UIAppDelegate
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
     return YES;
 }
-							
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -31,11 +36,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- Example/NavigationMenuController/main.m
+++ Example/NavigationMenuController/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "UIAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UIAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
