7c31998b3e91792a0442a9f13388b1e320b9bdc2
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-25 15:53:12 +0200

    Added class 'NavigationMenuOptionsController'

---
+++ Example/NavigationMenuController/NavigationMenuOptionsController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationMenuOptionsController.h
+//  NavigationMenuController
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationMenuOptionsController : UITableViewController
+
+@end
