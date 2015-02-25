ee71a84a050cb8828ab291548371438bae3fc299
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-25 11:26:58 +0200

    Added class 'NavigationMenuController' for example app

---
+++ Example/NavigationMenuController/NavigationMenuController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationMenuController.h
+//  NavigationMenuController
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import "UINavigationMenuController.h"
+
+@interface NavigationMenuController : UINavigationMenuController
+
+@end
