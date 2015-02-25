be504c1edb783b3d5d7729122211cc05c192150a
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-25 16:44:17 +0200

    Added class 'UINavigationMenuManager'

---
+++ Pod/Classes/UINavigationMenuManager.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationMenuManager.h
+//  Pods
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UINavigationMenuManager : NSObject
+
+@end
---
+++ Pod/Classes/UINavigationMenuManager.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationMenuManager.m
+//  Pods
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//
+//
+
+#import "UINavigationMenuManager.h"
+
+@implementation UINavigationMenuManager
+
+@end
