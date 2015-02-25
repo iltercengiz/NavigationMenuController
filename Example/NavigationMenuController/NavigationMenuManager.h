b8e990b4873125d95fc6b4b6ab801ac85578e225
Ilter Cengiz <iltercengiz@yahoo.com>
2015-02-25 15:33:08 +0200

    Added class 'NavigationMenuManager'

---
+++ Example/NavigationMenuController/NavigationMenuManager.h
@@ -0,0 +1,13 @@
+//
+//  NavigationMenuManager.h
+//  NavigationMenuController
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NavigationMenuManager : NSObject
+
+@end
---
+++ Example/NavigationMenuController/NavigationMenuManager.m
@@ -0,0 +1,13 @@
+//
+//  NavigationMenuManager.m
+//  NavigationMenuController
+//
+//  Created by Ilter Cengiz on 25/02/15.
+//  Copyright (c) 2015 Ilter Cengiz. All rights reserved.
+//
+
+#import "NavigationMenuManager.h"
+
+@implementation NavigationMenuManager
+
+@end
