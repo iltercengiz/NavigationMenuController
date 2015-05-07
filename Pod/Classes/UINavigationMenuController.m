81b68daa964c28bda16e43f239cd631ca19d2f42
Ilter Cengiz <iltercengiz@yahoo.com>
2015-05-07 15:06:42 +0300

    Removed LiveFrost from dependencies and used own blurring technique

--- Pod/Classes/UINavigationMenuController.m
+++ Pod/Classes/UINavigationMenuController.m
@@ -9,8 +9,6 @@
 #import "UINavigationMenuController.h"
 #import "UINavigationMenuManager.h"
 
-#import <LiveFrost/LiveFrost.h>
-
 @interface UINavigationMenuController () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate>
 
 /**
@@ -35,7 +33,7 @@
 /**
  This is used to blur the background content while showing the menu.
  */
-@property (nonatomic) LFGlassView *blurView;
+@property (nonatomic) UIView *blurView;
 
 /**
  These are used to hold references to bar buttons to hide/show them when showing/dismissing the menu.
@@ -72,34 +70,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    // Create the collection view
     self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0.0, 0.0, CGRectGetWidth(self.view.bounds), 0.0)
-                                             collectionViewLayout:[UICollectionViewFlowLayout new]];
+                                         collectionViewLayout:[UICollectionViewFlowLayout new]];
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
     self.collectionView.backgroundColor = [UIColor clearColor];
-    self.collectionView.contentInset = UIEdgeInsetsMake(64.0, 0.0, 0.0, 0.0);
+    self.collectionView.contentInset = UIEdgeInsetsMake(CGRectGetHeight(self.navigationBar.frame) + 20.0, 0.0, 0.0, 0.0);
     self.collectionView.hidden = YES;
+    self.collectionView.opaque = NO;
     self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"MenuItemCollectionViewCell"];
     [self.view insertSubview:self.collectionView belowSubview:self.navigationBar];
-    
-    self.blurView = [[LFGlassView alloc] initWithFrame:self.view.bounds];
+    // Create the blur view
+    if([UIBlurEffect class]) { // iOS 8
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+        self.blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+        self.blurView.frame = self.view.bounds;
+    } else { // workaround for iOS 7
+        self.blurView = [[UIToolbar alloc] initWithFrame:self.view.bounds];
+    }
     self.blurView.alpha = 0.0;
+    self.blurView.hidden = YES;
+    self.blurView.opaque = NO;
     self.blurView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view insertSubview:self.blurView belowSubview:self.collectionView];
-    
+    // Add constraints
     NSDictionary *views = @{@"collectionView": self.collectionView, @"blurView": self.blurView};
-    
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[collectionView]-0-|" options:0 metrics:nil views:views]];
-    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:-CGRectGetHeight(self.view.frame)];
-    [self.view addConstraint:constraint];
-    self.constraintCollectionViewTopToTop = constraint;
-    
-    constraint = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-CGRectGetHeight(self.view.frame)];
-    [self.view addConstraint:constraint];
-    self.constraintCollectionViewBottomToBottom = constraint;
-    
+    self.constraintCollectionViewTopToTop = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:-CGRectGetHeight(self.view.frame)];
+    [self.view addConstraint:self.constraintCollectionViewTopToTop];
+    self.constraintCollectionViewBottomToBottom = [NSLayoutConstraint constraintWithItem:self.collectionView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-CGRectGetHeight(self.view.frame)];
+    [self.view addConstraint:self.constraintCollectionViewBottomToBottom];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[blurView]-0-|" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[blurView]-0-|" options:0 metrics:nil views:views]];
 }
@@ -199,6 +201,7 @@
  */
 - (void)showMenu {
     self.collectionView.hidden = NO;
+    self.blurView.hidden = NO;
     self.constraintCollectionViewTopToTop.constant = 0.0;
     [self.view setNeedsLayout];
     [self.view layoutIfNeeded];
@@ -229,6 +232,7 @@
         self.blurView.alpha = 0.0;
     } completion:^(BOOL finished) {
         self.collectionView.hidden = YES;
+        self.blurView.hidden = YES;
         self.constraintCollectionViewTopToTop.constant = -CGRectGetHeight(self.view.frame);
     }];
     
